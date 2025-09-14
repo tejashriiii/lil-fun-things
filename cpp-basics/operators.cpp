8c7ad8329ccadf987c0e3c0c6597b52c65b4cdf3
tejashriiii <masketejashri11@gmail.com>
2025-09-14 15:25:43 +0530

    operators cpp

---
+++ cpp-basics/operators.cpp
@@ -0,0 +1,16 @@
+// this is vim btw.
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	double a=5,b=10,t;
+//	cout << a << endl;
+//	t=a;
+//	a=b;
+//	b=t;
+	t= (a + 10)/( 3 * b);
+	cout << t << endl;
+	return 0;
+
+}
