e4513a694765b5918e9a9aa5f972e9987d81b2cd
tejashriiii <masketejashri11@gmail.com>
2025-09-14 16:13:26 +0530

    basics

--- cpp-basics/operators.cpp
+++ cpp-basics/operators.cpp
@@ -1,5 +1,6 @@
 // this is vim btw.
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -9,8 +10,27 @@ int main(){
 //	t=a;
 //	a=b;
 //	b=t;
-	t= (a + 10)/( 3 * b);
-	cout << t << endl;
+//
+//	priority () >> * / >> + - , just like how in math.
+//	t= (a + 10)/( 3 * b);
+//	cout << t << endl;
+	
+	// with newtons 2nd law, f=ma, taking values
+	//double mass, acceleration,force;
+	//cout << "Enter Mass and Acceleration values:";
+	//cin >> mass >> acceleration;
+	//force = mass * acceleration;
+	//cout << "force is:" << force << "N" << endl;
+	
+
+	// area of cicle:
+	double radius=3, pi=3.1415, area;
+	area = pi * (pow(3,2));
+	cout << area << endl;
+
+
+	//you can fix variables by adding a 'const' as prefix
+	//uninitialized value will throw a random number, initialize with 0 or use {} while declaring;
 	return 0;
 
 }
